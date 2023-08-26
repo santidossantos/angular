2f8eb016e57acf745eed28e775c47dbcb258dfc5
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-08-26 13:00:07 -0300

    Cursos de Angular

---
+++ angular-basics/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { NgOptimizedImage } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
